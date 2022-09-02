99a0dff0c9e32cde4896943788895c9b5d1a5fac
ombiro96 <bakutarb@gmail.com>
2022-09-02 19:06:30 +0300

    small draft

---
+++ clifford.js
@@ -0,0 +1,21 @@
+let a=2
+let b=4
+
+let inputEl=document.getElementById("input-el")
+let inputEl1=document.getElementById("input-el1")
+let inputEl2=document.getElementById("input-el2")
+let inputEl3=document.getElementById("input-el3")
+let inputEl4=document.getElementById("input-el4")
+inputEl.textContent+="Clifford Ombiro Mwenda"
+function addition(){
+    inputEl1.textContent=a+b
+}
+function subtraction(){
+    inputEl2.textContent=a-b
+}
+function multiplication(){
+    inputEl3.textContent=a*b
+}
+function division(){
+    inputEl4.textContent=a/b
+}
